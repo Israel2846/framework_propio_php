8f19bb0e653ef2528d4b568dac426fff236529ae
Israel Colín <israaacolin@gmail.com>
2023-12-18 16:34:43 -0600

    Conexión con BD

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controllers;
 
+use App\Models\Contact;
+
 class HomeController extends Controller
 {
     public function index()
     {
+        $contactModel = new Contact();
+
         return $this->view('home', [
             'title' => 'Home',
             'description' => 'Esta es la página home'
