018f23edfd3b997b3e433f05ff77643404439b2f
Israel Colín <israaacolin@gmail.com>
2023-12-18 16:08:29 -0600

    Manejando vistas desde controladores

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,10 +2,10 @@
 
 namespace App\Controllers;
 
-class HomeController
+class HomeController extends Controller
 {
     public function index()
     {
-        return "Hola desde la página de inicio";
+        return $this->view('home');
     }
 }
